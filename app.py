c2369893c5ed792bb2a5c2b454dcd111dec63dfa
Azadova Indira <aindira2197@gmail.com>
2026-05-12 16:21:06 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+    ___tablename__ = 'blogs'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text)
+    create_at = db.Column(db.DateTime(), default=datetime.utcnow)
+    
+
+@app.route('/')
+def blog_list():
+    bloglar = Blog.query.all()
+    return render_template('index.html', bloglar=bloglar)
+
+
+@app.route('/blog/<int:id>/')
+def blog_detail(id):
+    blog = Blog.query.get(id)
+    return render_template('blog_detail.html', blog=blog)
+
+
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
